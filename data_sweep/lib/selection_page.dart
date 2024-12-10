2f82a4adedf7e19c969123fb8dbc46424159a7ac
Vincent <paulvincentcalvo.pr@gmail.com>
2024-12-10 18:01:17 +0800

    selection page changes

--- data_sweep/lib/selection_page.dart
+++ data_sweep/lib/selection_page.dart
@@ -23,150 +23,192 @@ class SelectionPages extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Selection Page"),
+        title: const Text(
+          "DATA SWEEP",
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 61, 126, 64),
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios,
+          size: 30,
+          color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
-      body: Padding(
+      body: Container(
+        color: const Color.fromARGB(255, 212, 216, 207),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Expanded(
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    _buildCard(
+                      context,
+                      "assets/DataCleaning.png", 
+                      "Fix inconsistencies for accurate analysis.",
+                      "Data Cleaning",
+                      () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => IssuesPage(
+                              csvData: csvData,
+                              columns: columns,
+                              classifications: classifications,
+                              casingSelections: casingSelections,
+                              dateFormats: dateFormats,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    _buildCard(
+                      context,
+                      "assets/HandleOutliers.png",
+                      "Detect and handle outliers to refine your dataset.",
+                      "Outliers",
+                      () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => OutliersPage(
+                              csvData: csvData,
+                              columns: columns,
+                              classifications: classifications,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    _buildCard(
+                      context,
+                      "assets/FeatureScaling.png", 
+                      "Normalize your data for better model performance.",
+                      "Feature Scaling",
+                      () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => FeatureScalingPage(
+                              csvData: csvData,
+                              columns: columns,
+                              classifications: classifications,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    _buildCard(
+                      context,
+                      "assets/DataVisualization.png",
+                      "Visualize your data.",
+                      "Data Visualization",
+                      () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => VisualizationPage(
+                              csvData: csvData,
+                              columns: columns,
+                              classifications: classifications,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard(
+    BuildContext context,
+    String imagePath,
+    String description,
+    String title,
+    VoidCallback onPressed,
+  ) {
+    return Container(
+      width: 320,
+      margin: EdgeInsets.only(right: 16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.white, Color(0xFF999999)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 8,
+            offset: Offset(2, 2),
+          ),
+        ],
+      ),
+      child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            Image.asset(
+              imagePath,
+              height: 300,
+              fit: BoxFit.contain,
+            ),
             Text(
-              "Ace, What do you want to do with the file?",
+              title,
               style: TextStyle(
-                fontWeight: FontWeight.bold,
                 fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
               ),
               textAlign: TextAlign.center,
             ),
-            SizedBox(height: 30),
+            Text(
+              description,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 12),
             ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => IssuesPage(
-                      csvData: csvData,
-                      columns: columns,
-                      classifications: classifications,
-                      casingSelections: casingSelections,
-                      dateFormats: dateFormats,
-                    ),
-                  ),
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Text(
-                      "Data Cleaning",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Clean your data by fixing inconsistencies for accurate analysis.",
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
+              onPressed: onPressed,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 61, 126, 64),
+                padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 30),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => OutliersPage(
-                      // NOT SURE KUNG ANO NEED MO. **PERA :<** dejok HHHAHA
-                      //Gawa nalng u ng function sa outlierspage para madiregard ang hindi numerical
-                      csvData: csvData,
-                      columns: columns,
-                      classifications: classifications, //CATEGORIES PER COLUMN
-                    ),
-                  ),
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Text(
-                      "Outliers",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Detect and handle outliers to refine your dataset.",
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FeatureScalingPage(
-                      csvData: csvData,
-                      columns: columns,
-                      classifications: classifications,
-                    ),
-                  ),
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Text(
-                      "Feature Scaling",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Normalize your data for better model performance.",
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => VisualizationPage(
-                      csvData: csvData,
-                      columns: columns,
-                      classifications: classifications,
-                    ),
-                  ),
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Text(
-                      "Data Visualization",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Visualize your Data",
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
+              child: const Text(
+                "Select",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
+                  color: Colors.white,
                 ),
               ),
             ),
